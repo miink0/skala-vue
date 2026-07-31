fd3c1096c8c5f88e4460bd71aeac9edfb18b39ca
miink0 <minye0812@naver.com>
2026-07-31 17:24:23 +0900

    1일차 과제

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-import TheWelcome from './components/TheWelcome.vue'
+import WeatherMockup from './components/WeatherMockup.vue'
 </script>
 
 <template>
-  <div style = "padding: 20px">
-    <TheWelcome />
+  <div style="padding: 20px">
+    <h1>과제 1: 날씨 Mockup</h1>
+    <WeatherMockup />
   </div>
 </template>
-
