7a70baaefa1f315deb4c8b83e2f4049fd7de9305
miink0 <minye0812@naver.com>
2026-08-04 11:22:40 +0900

    과제 4

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,18 @@ import ThemeBackground from './components/ThemeBackground.vue'
     </div>
     <div class="app-container">
       <h1>⛅ 과제 3: 날씨 (컴포넌트)</h1>
-      <hr />
       <WeatherParent />
     </div>
     <div class="app-container">
       <h1>⛅ 과제 4: 날씨 Router 활용</h1>
-      <hr />
-      <WeatherParent />
+      <nav class="navigation-bar">
+        <RouterLink to="/" class="nav-item"> 날씨 대시보드 </RouterLink>
+        <span class="nav-separator"> | </span>
+        <RouterLink to="/about" class="nav-item"> 날씨 프로젝트 소개 </RouterLink>
+      </nav>
+      <main>
+        <RouterView />
+      </main>
     </div>
   </ThemeBackground>
 </template>
